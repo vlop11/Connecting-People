ed3bd228f9d497e611869abdbe7354c36e8b828e
Emma Barron <emmalinbarron@gmail.com>
2018-07-23 11:09:07 -0400

    Template match
    made the templates in each class get() match the template that was declared for them.

--- main.py
+++ main.py
@@ -17,31 +17,31 @@ class LoginPage(webapp2.RequestHandler):
     def get(self):
         login_template = \
                 jinja_current_directory.get_template('templates/login.html')
-        self.response.write(home_template.render())
+        self.response.write(login_template.render())
 
 class FormPage(webapp2.RequestHandler):
         def get(self):
             form_template = \
                     jinja_current_directory.get_template('templates/form.html')
-            self.response.write(home_template.render())
+            self.response.write(form_template.render())
 
 class PeoplePage(webapp2.RequestHandler):
         def get(self):
             people_template = \
                     jinja_current_directory.get_template('templates/people.html')
-            self.response.write(home_template.render())
+            self.response.write(people_template.render())
 
 class ProfilePage(webapp2.RequestHandler):
         def get(self):
             profile_template = \
                     jinja_current_directory.get_template('templates/profile.html')
-            self.response.write(home_template.render())
+            self.response.write(profile_template.render())
 
 class UserProfilePage(webapp2.RequestHandler):
         def get(self):
             userprof_template = \
                     jinja_current_directory.get_template('templates/userprof.html')
-            self.response.write(home_template.render())
+            self.response.write(userprof_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
