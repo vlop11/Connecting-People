6dc6f008cee029ebc0ea9cdf04655cb63344cd86
Emma Barron <emmalinbarron@gmail.com>
2018-07-24 11:19:49 -0400

    uni num is a num
    uni num is  a nu so parseInt()

--- models.py
+++ models.py
@@ -2,8 +2,7 @@ from google.appengine.ext import ndb
 from google.appengine.api import users  # allows access to Google ID for log-in purposes
 
 class Interest(ndb.Model):
-    # the information for EachInterst object will go here
-    email = ndb.StringProperty(required=True)
+    # the information for Interst object will go here
     name = ndb.StringProperty(required=True)    # interest name                             ex. League of Legends
     alias = ndb.StringProperty(repeated=True)   # other names for interest                  ex. LoL or LOL
     number_of_users = ndb.IntegerProperty()     # how many people are interested in this    ex. 40
@@ -11,6 +10,9 @@ class Interest(ndb.Model):
 class User(ndb.Model):
     # user's name will be entered here
     name = ndb.StringProperty()
+
+    # user's email MUST be entered when it is created, and it goes here
+    email = ndb.StringProperty(required=True)
 
     #the image location will be entered here
     image = ndb.StringProperty()
