2413725bf87a615b8dcbd63b3ec4278f02ecd0d8
Valeria Lopez <val.lopez1011@gmail.com>
2018-07-27 09:22:26 -0400

    nvm

--- main.py
+++ main.py
@@ -102,11 +102,11 @@ class LoginPage(webapp2.RequestHandler):
 
         # Otherwise, the user isn't logged in to Google or us!
         else:
-            # self.response.write('''
-            #     Please log in to Google to use our site! <br>
-            #     <a href="%s">Sign in</a>''' % (
-            #       users.create_login_url('/login')))
-            self.redirect('/_ah/login?continue=http%3A//localhost%3A8080/login')
+            self.response.write('''
+                Please log in to Google to use our site! <br>
+                <a href="%s">Sign in</a>''' % (
+                  users.create_login_url('/login')))
+            # self.redirect('/_ah/login?continue=http%3A//localhost%3A8080/login')
 
     def post(self):
         user = users.get_current_user()
