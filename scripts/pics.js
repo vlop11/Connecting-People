3f6c69a04c78c09b69f78a53c9a03a27530a4036
Valeria <val.lopez1011@gmail.com>
2018-08-27 12:07:51 -0400

    smoother bkgd images
    fixed some of the javascript to make the transitions between background images smoother

--- scripts/pics.js
+++ scripts/pics.js
@@ -1,20 +1,43 @@
-var images = [
-  "../images/bank.jpg",
-  "../images/georgiatech.jpg",
-  "../images/omni.jpg"
+// var images = [
+//   "../images/bank.jpg",
+//   "../images/georgiatech.jpg",
+//   "../images/omni.jpg"
+//
+// ]
+//
+// var imageHead = document.getElementsByClassName("html_image")[0];
+// var i = 0;
+// // here, html_image is a list so grabbing the
+//
+// setInterval(function() {
+//       // console.log(imageHead)
+//       imageHead.style.backgroundImage = "url(" + images[i] + ")";
+//       i = i + 1;
+//       if (i == images.length) {
+//       	i =  0;
+//         // this restarts the loop
+//       }
+// }, 5000);
 
-]
+//new method for changing background pics
+//it's a lot smoother
+var bgImageArray = ["bank.jpg", "georgiatech.jpg", "omni.jpg"],
+base = "../images/",
+secs = 5;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img;
+    // caches images, avoiding white flash between background replacements
+});
 
-var imageHead = document.getElementsByClassName("html_image")[0];
-var i = 0;
-// here, html_image is a list so grabbing the
-
-setInterval(function() {
-      // console.log(imageHead)
-      imageHead.style.backgroundImage = "url(" + images[i] + ")";
-      i = i + 1;
-      if (i == images.length) {
-      	i =  0;
-        // this restarts the loop
-      }
-}, 5000);
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){
+			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }
+		}, (secs * 1000) * i)
+	}
+}
+backgroundSequence();
